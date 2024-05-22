ebcfd926f294aa1e3d499147ae986ba74ee985d9
Mikita Shydlouski <retdarkw@gmail.com>
2024-05-22 12:23:40 +0300

    feat: add task 7

--- public/index.php
+++ public/index.php
@@ -2,35 +2,12 @@
 
 declare(strict_types=1);
 
-$arr1 = [
-    'must' => 'test',
-    'important' => 'family',
-    'php' => 'interesting',
-    'dump' => 'test'
+$numerics = [
+    'grades' => range(1, 20),
+    'scores' => range(20, 1),
 ];
 
-print_r($arr1);
-echo '<br><br>';
-
-$keys = array_keys($arr1);
-
-print_r($keys);
-echo '<br><br>';
-
-$keys = array_keys($arr1, 'test',true);
-
-print_r($keys);
-echo '<br><br>';
-
-$values = array_values($arr1);
-
-print_r($values);
-echo '<br><br>';
-
-foreach ($keys as $key){
-    $arr1[$key] = 'new value';
+foreach ($numerics as $grades) {
+    print_r(array_map(fn($value): int => $value * $value, $grades));
+    echo '<br><br>';
 }
-
-echo 'After changing<br>';
-print_r($arr1);
-echo '<br><br>';
