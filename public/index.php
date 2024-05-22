8bf49b3c04f01d0f7eae9b3482f39c69750996c6
Mikita Shydlouski <retdarkw@gmail.com>
2024-05-22 10:09:05 +0300

    feat: add task4

--- public/index.php
+++ public/index.php
@@ -2,37 +2,22 @@
 
 declare(strict_types=1);
 
-require_once 'vendor/autoload.php';
-
-use Psr\Log\LoggerInterface;
-use App\Logger\Logger;
-
-function get_result_message(mixed $value, string $search_string): string
-{
-    if ($value === false){
-        $message = sprintf('no %s in array was', $search_string);
-    }else{
-        $message = sprintf('found %s in array at index %s', $search_string, $value);
-    }
-
-    return $message;
-}
-
-function searching(string $search, array $strings, LoggerInterface $logger): void
-{
-    $res = array_search($search, $strings, true);
-    $message = get_result_message($res, $search);
-
-    $logger->info($message);
-}
-
-$logger = new Logger('default');
-
-$data = [
-    'mom', 'dad', 'brother', 'sister', 'aunt', 'uncle',
-    'red', 'blue', 'white', 'white beard', 'mom cool'
+$arr1 = [
+    'parents' => array('mom', 'dad'),
+    'key' => array(array('value 1', 'value 2'), 1),
+    'value'
 ];
 
-searching('mom cool', $data, $logger);
+$arr2 = [
+    range(1, 12),
+    'parents' => array('mom 1', 'dad 2'),
+];
 
+$res = array_merge_recursive($arr1, $arr2);
 
+print_r($res);
+echo '<br><br>';
+
+$res = array_merge($arr1, $arr2);
+print_r($res);
+echo '<br><br>';
