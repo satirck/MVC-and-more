1d0a154cc5408c270ca0443b456e46a241e59f9a
Mikita Shydlouski <retdarkw@gmail.com>
2024-06-05 16:02:39 +0300

    feat: add completed task2 from block base operations in theme formating, validation, text deciration

--- public/index.php
+++ public/index.php
@@ -2,93 +2,51 @@
 
 declare(strict_types=1);
 
-function isValidEmail(string $email): bool
+function isPasswordStrong($password): bool
 {
-    $pattern = '/(^\S+)@(\S+)\.(\S+$)/';
-
-    return preg_match($pattern, $email) === 1;
+    $pattern = '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$/';
+    return !(preg_match($pattern, $password) === false);
 }
 
-function parseFromCapital($string): array
+function isValidIPAddress($ip): bool
 {
-    $pattern = '/([A-Z].+)/m';
+    $pattern = '/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/';
+    preg_match($pattern, $ip, $matches);
 
-    preg_match_all($pattern, $string, $matches);
-    return $matches[0];
+    if (count($matches) == 5) {
+        $parts = array_slice($matches, 1);
+
+        $filteredParts = array_filter($parts, function ($part) {
+            return $part >= 0 && $part <= 255;
+        });
+
+        return count($filteredParts) == 4;
+    }
+    return false;
 }
 
-function extractUrls(string $html): array
-{
-    $pattern = '/https?:\/\/[^\s"<>\']+/i';
+$phone = "79123456789";
 
-    preg_match_all($pattern, $html, $matches);
+$pattern = '/(\d)(\d{3})(\d{3})(\d{4})/';
+preg_match($pattern, $phone, $matches);
 
-    return $matches[0];
-}
+$resPhone = sprintf('+%s (%s) %s-%s', $matches[1], $matches[2], $matches[3], $matches[4]);
+echo sprintf('Orig: [%s] now: [%s]<br>', $phone, $resPhone);
 
-function extractBetweenSymbols(string $text): array
-{
-    $pattern = '/(?<=,)[^,]+(?=,)/';
+$password = "YourP@ssw0rd";
+echo sprintf('Your password [%s] is %ssecure<br>', $password, isPasswordStrong($password) ? '' : 'not ');
 
-    preg_match_all($pattern, $text, $matches);
+$ip = "192.300.1.1";
+echo sprintf('Your ip [%s] is %scorrect<br>', $ip, isValidIPAddress($ip) ? '' : 'not ');
 
-    return $matches[0];
-}
+$text = "Этот текс содержит ошибки, таке как опечатка в слове текс или неверное написание слова таке.";
 
-function replaceToString(string $first, string $toSearch, string $toReplace): string
-{
-    return preg_replace($toSearch, $toReplace, $first);
-}
+$corrections = array(
+    'текс' => 'текст',
+    'таке' => 'такие'
+);
 
-$html_string = '
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Hello world
-    <a href="http://localhost"></a>
-    <a href="https://vk.com"></a>
-</body>
-</html>';
+$newText = preg_replace('/текс/', 'текст', $text);
+$newText = preg_replace('/тaке/', 'такие', $newText);
 
-$email1 = 'retdarkw@gmail.com';
-$email2 = 'retdarkw.gmail@.com';
-
-$string = sprintf('Cool day%sCall me%scoach%sletter', PHP_EOL, PHP_EOL, PHP_EOL);
-$string2 = 'Hello, me friend, hello, cool day. Can we speak together, a, cool.';
-$string3 = 'Говоря привет, мы вежливо сообщаем что рады видеть человека. 
-Даже такое обращение как привет, может вызвать улыбку.';
-
-echo 'Mails: <br>';
-
-echo sprintf('Mail [%s] is %s mail<br>', $email1, isValidEmail($email1) ? 'valid' : 'invalid');
-echo sprintf('Mail [%s] is %s mail<br><br>', $email2, isValidEmail($email2) ? 'valid' : 'invalid');
-
-echo sprintf('Getting capitals from <br>%s', str_replace(PHP_EOL, '<br>', $string));
-
-$capitals = parseFromCapital($string);
-print_r($capitals);
-echo '<br>';
-
-$escaped_html = htmlspecialchars($html_string, ENT_QUOTES, 'UTF-8');
-$urls = extractUrls($html_string);
-
-echo sprintf('Original: %s<br><br>', $escaped_html);
-echo 'Extracted urls: <br>';
-print_r($urls);
-echo '<br><br>';
-
-$quBetween = extractBetweenSymbols($string2, ',');
-
-echo sprintf('Original: %s<br>', $string2);
-echo 'Extracted between [,] : <br>';
-print_r($quBetween);
-echo '<br>';
-
-$newString3 = replaceToString($string3, '/привет/', 'здравствуй');
-echo sprintf('Original: %s<br>', $string3);
-echo sprintf('Replace [привет] на [здравствуй] : <br> %s<br>', $newString3);
+echo $newText;
